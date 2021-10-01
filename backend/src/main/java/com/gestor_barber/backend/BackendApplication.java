fe69c39fd1e5d4322abdbe63af0a823ce28bad99
Breno <brenovinicius545@gmail.com>
2021-10-01 20:43:34 -0300

    Adição do projeto backend com configuração do database e do liquibase

---
+++ backend/src/main/java/com/gestor_barber/backend/BackendApplication.java
@@ -0,0 +1,18 @@
+package com.gestor_barber.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendApplication.class, args);
+		System.out.println("========================================================");
+		System.out.println("========================================================");
+		System.out.println("===================Serviço iniciado=====================");
+		System.out.println("========================================================");
+		System.out.println("========================================================");
+	}
+
+}
